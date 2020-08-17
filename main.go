4005b490b663230cf6d3f0946de546213248b114
Michael Vasiliev <44232479+mikeitup@users.noreply.github.com>
2020-08-17 13:50:52 +0200

    v4

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World v3"}
+		res := &response{Message: "Hello World v4"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
