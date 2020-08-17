b404f9b5ed31c537ddee8de1632330cf463bb2b4
Michael Vasiliev <44232479+mikeitup@users.noreply.github.com>
2020-08-17 14:13:59 +0200

    Big changes in v5

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World v4"}
+		res := &response{Message: "Hello World v5"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
